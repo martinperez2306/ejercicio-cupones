d9b248afcda6abe6d4f644881d5fa333561374e5
Martin Nicolas Perez <perezmartincontacto@gmail.com>
2021-01-14 19:02:11 -0300

    Agrego inyeccion de repositorios segun propiedad de archivo de configuracion

--- coupons-apirest/src/main/java/org/mperez/coupons/reposirory/ItemMemory.java
+++ coupons-apirest/src/main/java/org/mperez/coupons/reposirory/ItemMemory.java
@@ -5,11 +5,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.mperez.coupons.factory.ItemsFactory;
 import org.mperez.coupons.model.Item;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@ConditionalOnProperty(value="repository.item", havingValue = "mem")
 public class ItemMemory implements ItemRepository, InitializingBean {
 	
 	private Map<String, Item> itemsById;
@@ -35,7 +38,6 @@ public class ItemMemory implements ItemRepository, InitializingBean {
 		return items;
 	}
 
-
 	@Override
 	public List<Item> findByMaxAmount(Float maxAmount) {
 		// TODO Auto-generated method stub
@@ -57,10 +59,19 @@ public class ItemMemory implements ItemRepository, InitializingBean {
 	public Boolean deleteById(String itemId) {
 		return itemsById.remove(itemId) != null;
 	}
+	
+	private void initData() {
+		itemsById.put("MLA1", ItemsFactory.createItem("MLA1", 100));
+		itemsById.put("MLA1", ItemsFactory.createItem("MLA2", 210));
+		itemsById.put("MLA3", ItemsFactory.createItem("MLA3", 260));
+		itemsById.put("MLA4", ItemsFactory.createItem("MLA4", 80));
+		itemsById.put("MLA5", ItemsFactory.createItem("MLA5", 90));
+	}
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		this.itemsById = new HashMap<String, Item>();
+		initData();
 	}
 
 }
