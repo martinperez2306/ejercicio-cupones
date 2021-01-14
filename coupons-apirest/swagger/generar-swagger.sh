670d318e869a5a0905583ca510ee4b1723712aa3
Martin Nicolas Perez <perezmartincontacto@gmail.com>
2021-01-14 16:36:49 -0300

    Agrego script de generacion de codigo y codegen

---
+++ coupons-apirest/swagger/generar-swagger.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -rf generated/
+java -jar swagger-codegen-cli.jar generate -i cupones.yaml --api-package org.mperez.coupons.rest.api --model-package org.mperez.coupons.rest.model -l spring -o generated/
