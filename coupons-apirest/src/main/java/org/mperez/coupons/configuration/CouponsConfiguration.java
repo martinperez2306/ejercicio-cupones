b674a9f078bdd8395ab101c391adbf38c581924e
Martin Nicolas Perez <perezmartincontacto@gmail.com>
2021-01-15 01:05:06 -0300

    Configuracion para Bean de Spring del Cliente de la Api de ITEMS

--- coupons-apirest/src/main/java/org/mperez/coupons/configuration/CouponsConfiguration.java
+++ coupons-apirest/src/main/java/org/mperez/coupons/configuration/CouponsConfiguration.java
@@ -2,15 +2,25 @@ package org.mperez.coupons.configuration;
 
 import org.mperez.coupons.calculation.ItemsCalculation;
 import org.mperez.coupons.calculation.MaximizeTotalSpent;
+import org.mperez.items.api.client.ItemApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CouponsConfiguration {
 	
+	@Value("{items.api.baseurl}")
+	private String itemsApiBaseUrl;
+	
 	@Bean
-	public ItemsCalculation steamClient() {
+	public ItemsCalculation itemsCalculation() {
 		return new MaximizeTotalSpent();
+	}
+	
+	@Bean
+	public ItemApi itemApi() {
+		return new ItemApi(itemsApiBaseUrl);
 	}
 	
 }
