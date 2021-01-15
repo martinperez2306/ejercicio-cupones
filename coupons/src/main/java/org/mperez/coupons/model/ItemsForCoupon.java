a31d173908e084e3b3681c61fc640ce5f8d16a7e
Martin Nicolas Perez <perezmartincontacto@gmail.com>
2021-01-15 18:29:21 -0300

    Agrego estructura de datos ItemsForCoupon

---
+++ coupons/src/main/java/org/mperez/coupons/model/ItemsForCoupon.java
@@ -0,0 +1,29 @@
+package org.mperez.coupons.model;
+
+import java.util.List;
+
+public class ItemsForCoupon {
+	
+	private List<String> itemIds;
+	private Float total;
+	private Coupon coupon;
+	
+	public ItemsForCoupon(List<String> itemIds, Float total,Coupon coupon) {
+		this.itemIds = itemIds;
+		this.total = total;
+		this.coupon = coupon;
+	}
+
+	public List<String> getItemIds() {
+		return itemIds;
+	}
+
+	public Float getTotal() {
+		return total;
+	}
+
+	public Coupon getCoupon() {
+		return coupon;
+	}
+
+}
