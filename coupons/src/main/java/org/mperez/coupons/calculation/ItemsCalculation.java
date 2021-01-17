07e9c2fc71893e728bd497e2a401b86ff454f2b4
Martin Nicolas Perez <perezmartincontacto@gmail.com>
2021-01-16 22:32:10 -0300

    Agrego documentacion, descripcion, pre y post condiciones

--- coupons/src/main/java/org/mperez/coupons/calculation/ItemsCalculation.java
+++ coupons/src/main/java/org/mperez/coupons/calculation/ItemsCalculation.java
@@ -3,8 +3,44 @@ package org.mperez.coupons.calculation;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Representa el Calculo de Items que un usuario puede comprar con un Cupon de
+ * cierto monto maximizando el total gastado entre todos los items sin superar
+ * el monto del cupon.
+ * 
+ * @author mperez
+ *
+ */
 public interface ItemsCalculation {
-	
+
+	/**
+	 * <h1>Calculo de Items</h1>
+	 * 
+	 * Dado un conjunto de Items y un monto total, regresa un listado de item Ids
+	 * que pueden ser comprados sin superar el monto total. El listado de item IDs
+	 * maximiza el total gastado considerando el precio de todos sus items, el cual
+	 * puede ser menor o igual al monto total permitido. Si existe mas de un listado
+	 * de items que asegure el gasto maximo, se devuelve el <strong>ultimo</strong>
+	 * listado de items encontrado durante el calculo.
+	 * 
+	 * <p>
+	 * 
+	 * <strong>PRE:</strong> 
+	 * Recibe los items que el usuario puede comprar y el
+	 * monto total que puede gastar. <code>items</code> no puede ser null.
+	 * <code>amount</code> no puede ser null.
+	 * 
+	 * <p>
+	 * 
+	 * <strong>POST:</strong> 
+	 * Devuelve el listado con todos los itemsIDs que el
+	 * usuario puede comprar sin superar el monto total. El resultado puede ser una
+	 * lista de String vacia.
+	 * 
+	 * @param items Mapa de Items. La clave es el itemId y el valor es el precio del Item. 
+	 * @param amount Monto total que se puede gastar entre todos los items.
+	 * @return
+	 */
 	public List<String> calculate(Map<String, Float> items, Float amount);
 
 }
--- coupons/src/main/java/org/mperez/coupons/utils/ItemUtils.java
+++ coupons/src/main/java/org/mperez/coupons/utils/ItemUtils.java
@@ -10,17 +10,57 @@ import org.mperez.coupons.model.Item;
 
 public class ItemUtils {
 	
+	/**
+	 * <h1> Obtener el item de Precio Maximo </h1>
+	 * 
+	 * Dado una lista de items regresa el item con mayor precio.
+	 * <p>
+	 * <strong>PRE:</strong> 
+	 * Recibe el listado de items. <code>items</code> no puede ser null o bien un listado vacio.
+	 * <p>
+	 * <strong>POST:</strong> 
+	 * Devuelve el item con mayor precio entre todo el listado de items.
+	 * @param items
+	 * @return
+	 */
 	public static Item getItemWithMaxPrice(List<Item> items) {
 		return items.stream().reduce((item1,item2) -> item1.getAmount() >= item2.getAmount() ? item1 : item2).get();
 	}
 	
+	/**
+	 * <h1> Obtener Items con precio menor a un Monto determinado </h1>
+	 * 
+	 * Dado una lista de items y un monto regresa un sublistado de items que no superen dicho monto.
+	 * <p>
+	 * <strong>PRE:</strong> 
+	 * Recibe el listado de items. 
+	 * <code>items</code> no puede ser null o bien un listado vacio.
+	 * <code>amount</code> no puede ser null.
+	 * <p>
+	 * <strong>POST:</strong> 
+	 * Devuelve el item con mayor precio entre todo el listado de items.
+	 * @param items
+	 * @param amount
+	 * @return
+	 */
 	public static List<Item> getItemsWithPriceLowerThanAmount(Map<String, Float> items, Float amount){
 		List<Item> itemList = new ArrayList<Item>();
 		items.forEach((i,a) -> itemList.add(ItemsFactory.createItem(i, a)));
-		return itemList.stream().filter(item -> itemPriceIsLessThanAmount(item, amount)).collect(Collectors.toList());
+		return itemList.stream().filter(item -> itemPriceIsLessOrEqualsThanAmount(item, amount)).collect(Collectors.toList());
 	}
 	
-	public static Boolean itemPriceIsLessThanAmount(Item item, Float amount) {
+	/**
+	 * <strong>PRE:</strong> 
+	 * <code>item</code> no puede ser null.
+	 * <code>amount</code> no puede ser null.
+	 * <p>
+	 * <strong>POST:</strong>
+	 * Regresa <code>true</code> si el Precio del Item no supera el Monto, <code>false</code> en otro caso.
+	 * @param item
+	 * @param amount
+	 * @return
+	 */
+	public static Boolean itemPriceIsLessOrEqualsThanAmount(Item item, Float amount) {
 		return item.getAmount() <= amount;
 	}
 
